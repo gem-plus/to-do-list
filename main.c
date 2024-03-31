71573f248b579839521ad05b07df112a31c8330f
gem-plus <chauhan.aditya.work@gmail.com>
2024-03-31 20:40:54 +0530

    few improvement

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_EVENT_SIZE 100
+#define MAX_TIME_SIZE 10
+
 int main()
 {
     FILE *file_ptr;
-    char tme[10];
-    char str[100];
-    int minutes , hour;
-
+    char tme[MAX_TIME_SIZE];
+    char str[MAX_EVENT_SIZE];
+    int minutes, hour;
 
     file_ptr = fopen("to-do-list.txt", "w");
 
@@ -18,21 +20,36 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    while (1)
+    {
+        printf("Enter Time (HH:MM): ");
+        fgets(tme, sizeof(tme), stdin);
+        int minutes, hour;
 
-    printf("Enter Time (HH:MM): ");
-    fgets(tme, sizeof(tme), stdin);
-    if (sscanf(tme, "%d:%d", &hour, &minutes)!= 2 || (hour < 0 || hour > 23) || (minutes < 0 || minutes > 59)){
-        printf("Invalid Time Format !\n");
-        exit(EXIT_FAILURE);
+        if (sscanf(tme, "%d:%d", &hour, &minutes) != 2 || (hour < 0 || hour > 23) || (minutes < 0 || minutes > 59))
+        {
+            printf("Invalid Time Format !\n");
+            continue;
+        }
+
+        printf("Enter Event: ");
+        fgets(str, sizeof(str), stdin);
+
+        tme[strlen(tme) - 1] = '\0';
+        str[strlen(str) - 1] = '\0';
+
+        fprintf(file_ptr, "%s :- %s\n", tme, str);
+
+        printf("Do you wish to add another event (y/n): ");
+        char choice;
+        scanf(" %c", &choice);
+        if (choice != 'y' && choice != 'Y')
+            break;
+
+        while (getchar() != '\n')
+            ;
+        printf("\n");
     }
-
-    printf("Enter Event: ");
-    fgets(str, sizeof(str), stdin);
-
-    tme[strlen(tme) - 1] = '\0';
-    str[strlen(str) - 1] = '\0';
-
-    fprintf(file_ptr, "%s : %s\n", tme, str);
     fclose(file_ptr);
     return 0;
 }
